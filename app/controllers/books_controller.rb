37979d5aefb4306565e6b5193641a4a97605567c
yurika0501 <119822953+yurika0501@users.noreply.github.com>
2023-01-08 14:26:49 +0000

    [Add]create機能、editページ作成、update機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,10 +6,12 @@ class BooksController < ApplicationController
 
   # 2章参考
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    # １. データを受け取り新規登録するためのインスタンス作成
     book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
+    # 2. データをデータベースに保存するためのsaveメソッド実行
     book.save
+    # 3フラッシュメッセ
+    flash[:notice] = "Submission was successfully"
     # 4. books/id(idはURLで数字になる)画面へリダイレクト
     redirect_to book_path(book.id)
   end
@@ -19,16 +21,27 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books = Book.all
+    
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+    
 
   # 7章参考
   def destroy
-    book = Book.find(params[:id]) # データ（レコード）を1件取得
+    @book = Book.find(params[:id]) # データ（レコード）を1件取得
     book.destroy # データ（レコード）を削除
-    redirect_to '/books' # 投稿一覧画面へリダイレクト
+    redirect_to book_path(book.id) # 投稿一覧画面へリダイレクト
   end
 
   private
