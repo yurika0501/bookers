bb6f6b3f4ef0a23d9f99ce460d7f4c502da1a142
yurika0501 <119822953+yurika0501@users.noreply.github.com>
2023-01-09 13:05:00 +0000

    [Add]バリデーション実装/フラッシュメッセージ実装/destroy機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,43 +5,55 @@ class BooksController < ApplicationController
   end
 
   # 2章参考
+  # データを追加（保存）する
   def create
     # １. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # 2. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    if @book.save
     # 3フラッシュメッセ
-    flash[:notice] = "Submission was successfully"
+      flash[:notice] = "Data Storage was successfully"
     # 4. books/id(idはURLで数字になる)画面へリダイレクト
-    redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
     @books = Book.all
-    
+
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+    # フラッシュメッセ。更新に成功
+      flash[:notice] = "Update successfully"
+      redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
-    
+
 
   # 7章参考
   def destroy
-    @book = Book.find(params[:id]) # データ（レコード）を1件取得
+    book = Book.find(params[:id]) # データ（レコード）を1件取得
     book.destroy # データ（レコード）を削除
-    redirect_to book_path(book.id) # 投稿一覧画面へリダイレクト
+    redirect_to books_path # 投稿一覧画面へリダイレクト
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
-  resources :books
+  get '/' => 'homes#top'
+  resources :books, except:[:destroy]
+  delete '/books/:id', to: 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# topをなくすので試してみたコード
+# root to: 'home#top'
